c051d782e7a39bc0969b3e1dd43228eca8ce0d92
ayomahansani <ayohansi17@gmail.com>
2025-02-28 12:39:07 +0530

    change server.ts

--- server.ts
+++ server.ts
@@ -11,11 +11,7 @@ app.use(express.json());  //for parsing application/json
 
 app.use(cors());
 
-console.log("Loaded SECRET_KEY:", process.env.SECRET_KEY);
-
 app.use('/auth', authRoutes);
-
-app.use(authenticateToken);
 
 app.listen(3001, (err =>{
     console.log("Server running on port 3001");
